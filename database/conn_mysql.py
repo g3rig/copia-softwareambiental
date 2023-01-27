d2fcd950ae57c1b9b3b1fc37d81aa806a0fbf417
Germán <germanignacio.92@gmail.com>
2023-01-27 14:49:06 -0300

    edit conn_open

--- database/conn_mysql.py
+++ database/conn_mysql.py
@@ -13,9 +13,9 @@ def get_connection():
   return conn
 
 def insert_mul1(data_mul1):
-  conn_open = True
   try:
     conn = get_connection()
+    conn_open = True
     with conn:
       with conn.cursor() as cursor:
         query = 'INSERT INTO mul_1(fecha_m1, hora_m1, p1tmt1, p1tmt2, p1tmt3, p1tmt4, p1tp1, p1tp2, p1tp3, p1tp4, p1tp5, p1tl1, p1ts3, p1ts1, p1ts2, p2co2_2, p1co2_1, p2co2_1) VALUES %s'
@@ -29,9 +29,9 @@ def insert_mul1(data_mul1):
       conn_open = False
 
 def insert_mul2(data_mul2):
-  conn_open = True
   try:
     conn = get_connection()
+    conn_open = True
     with conn:
       with conn.cursor() as cursor:
         query = 'INSERT INTO mul_2(fecha_m2, hora_m2, p2tmt1, p2tmt2, p2tmt3, p2tmt4, p2tmt5, p2tmt6, p2tt1, p2tt2, p2tt3, p2tt4, p2tt5, p2tt6, p2tt7, co2au, tempau, hrau) VALUES %s'
