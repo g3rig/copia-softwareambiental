69409beee8fd17be81bdf0ebf9f3b4baa8adaed9
Germán <germanignacio.92@gmail.com>
2023-01-27 15:59:09 -0300

    add query SQL SELECT

---
+++ database/conn_bd.py
@@ -0,0 +1,22 @@
+import mysql.connector
+from decouple import config
+
+conn = mysql.connector.connect(
+  host=config('HOST'),
+  user=config('USER'),
+  password=config('PASSWORD'),
+  db=config('DBNAME')
+)
+
+cursor = conn.cursor()
+
+query = "SELECT * FROM mul_1 ORDER BY id_reg_mul1 DESC limit 1"
+cursor.execute(query)
+
+res = cursor.fetchallall()
+
+for resultado in res:
+  print(res)
+
+cursor.close()
+conn.close()
